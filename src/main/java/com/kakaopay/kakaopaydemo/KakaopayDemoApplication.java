31402426651a7f83a1e9ceabbc7cd9742dc23391
ChangYong Lee <changyonglee@ChangYongui-MacBookPro.local>
2021-12-12 13:06:52 +0900

    initialize

---
+++ src/main/java/com/kakaopay/kakaopaydemo/KakaopayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.kakaopaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaopayDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakaopay/kakaopaydemo/KakaopayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopay.kakaopaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
